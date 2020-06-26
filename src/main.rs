89aa1a2d80e32501930f9ab8c06b6d8fe98799f2
Sven-Hendrik Haase <svenstaro@gmail.com>
2020-06-27 00:51:35 +0200

    Run cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
-extern crate anyhow;
-extern crate strum;
-extern crate strum_macros;
-
-use args::*;
 mod args;
+use args::*;
 
 mod errors;
 use errors::*;
@@ -21,9 +17,9 @@ use nix::sys::statfs;
 
 use env_logger::Env;
 
+use anyhow::Result;
 use colored::*;
 use structopt::StructOpt;
-use anyhow::Result;
 
 pub fn format_count(num: f64, delimiter: f64) -> String {
     let units = ["B", "k", "M", "G", "T", "P", "E", "Z", "Y"];
@@ -53,8 +49,9 @@ fn bar(width: usize, percentage: Option<f32>, theme: &Theme) -> String {
 
     let color_empty = match percentage {
         Some(_) => theme.color_usage_low,
-        None => theme.color_usage_void
-    }.unwrap_or(Color::Green);
+        None => theme.color_usage_void,
+    }
+    .unwrap_or(Color::Green);
 
     let fill_low = theme
         .char_bar_filled
@@ -108,8 +105,16 @@ fn display_mounts(mnts: &[&MountEntry], theme: &Theme, inodes_mode: bool) {
     let fsname_width = column_width(&mnts, &|m: &&MountEntry| m.mnt_fsname.len(), label_fsname);
     let type_width = column_width(&mnts, &|m: &&MountEntry| m.mnt_type.len(), label_type);
     let used_width = column_width(&mnts, &|m: &&MountEntry| m.used_formatted.len(), label_used);
-    let available_width = column_width(&mnts, &|m: &&MountEntry| m.free_formatted.len(), label_available);
-    let capacity_width = column_width(&mnts, &|m: &&MountEntry| m.capacity_formatted.len(), label_capacity);
+    let available_width = column_width(
+        &mnts,
+        &|m: &&MountEntry| m.free_formatted.len(),
+        label_available,
+    );
+    let capacity_width = column_width(
+        &mnts,
+        &|m: &&MountEntry| m.capacity_formatted.len(),
+        label_capacity,
+    );
 
     println!(
         "{:<fsname_width$} {:<type_width$} {:<bar_width$} {:>6} {:>used_width$} {:>available_width$} {:>capacity_width$} {}",
@@ -134,12 +139,18 @@ fn display_mounts(mnts: &[&MountEntry], theme: &Theme, inodes_mode: bool) {
             Some(p) if p >= theme.threshold_usage_medium => theme.color_usage_medium,
             Some(_) => theme.color_usage_low,
             _ => theme.color_usage_void,
-        }.unwrap_or(Color::White);
+        }
+        .unwrap_or(Color::White);
 
         let used_percentage = match mnt.used_percentage {
-            Some(percentage) => format!("{:>5.1}{}", (percentage * 10.0).round() / 10.0, "%".color(Color::White)),
-            None => format!("{:>6}", "-")
-        }.color(color_usage);
+            Some(percentage) => format!(
+                "{:>5.1}{}",
+                (percentage * 10.0).round() / 10.0,
+                "%".color(Color::White)
+            ),
+            None => format!("{:>6}", "-"),
+        }
+        .color(color_usage);
 
         println!(
             "{:<fsname_width$} {:<type_width$} {} {} {:>used_width$} {:>available_width$} {:>size_width$} {}",
@@ -164,13 +175,13 @@ fn run(args: Args) -> Result<()> {
     if let Some(color) = args.color {
         debug!("Bypass tty detection for colors: {:?}", color);
         match color {
-            ColorOpt::Auto => {},
+            ColorOpt::Auto => {}
             ColorOpt::Always => {
                 colored::control::set_override(true);
-            },
+            }
             ColorOpt::Never => {
                 colored::control::set_override(false);
-            },
+            }
         }
     }
 
@@ -184,7 +195,11 @@ fn run(args: Args) -> Result<()> {
         _ => {
             let theme = Theme::new();
             let f = File::open("/proc/self/mounts")?;
-            let delimiter = if args.base10 { NumberFormat::Base10 } else { NumberFormat::Base2 };
+            let delimiter = if args.base10 {
+                NumberFormat::Base10
+            } else {
+                NumberFormat::Base2
+            };
 
             let mounts_to_show = if args.all {
                 DisplayFilter::All
@@ -195,16 +210,19 @@ fn run(args: Args) -> Result<()> {
             };
 
             let mnts = get_mounts(f)?;
-            let mut mnts:Vec<MountEntry> = mnts
+            let mut mnts: Vec<MountEntry> = mnts
                 .into_iter()
                 .filter(|mount| {
-                    mounts_to_show.get_mnt_fsname_filter().iter().any(|&fsname| {
-                        if fsname.ends_with('*') {
-                            mount.mnt_fsname.starts_with(&fsname[..fsname.len() - 1])
-                        } else {
-                            mount.mnt_fsname == fsname
-                        }
-                    })
+                    mounts_to_show
+                        .get_mnt_fsname_filter()
+                        .iter()
+                        .any(|&fsname| {
+                            if fsname.ends_with('*') {
+                                mount.mnt_fsname.starts_with(&fsname[..fsname.len() - 1])
+                            } else {
+                                mount.mnt_fsname == fsname
+                            }
+                        })
                 })
                 .collect::<Vec<_>>();
 
@@ -218,10 +236,10 @@ fn run(args: Args) -> Result<()> {
                         } else {
                             (
                                 stat.blocks() as u64 * (stat.block_size() as u64),
-                                stat.blocks_available() as u64 * (stat.block_size() as u64)
+                                stat.blocks_available() as u64 * (stat.block_size() as u64),
                             )
                         }
-                    },
+                    }
                     None => (0, 0),
                 };
 
@@ -229,7 +247,8 @@ fn run(args: Args) -> Result<()> {
                 mnt.free = free;
                 mnt.used = capacity - free;
 
-                mnt.capacity_formatted = format_count(mnt.capacity as f64, delimiter.get_powers_of());
+                mnt.capacity_formatted =
+                    format_count(mnt.capacity as f64, delimiter.get_powers_of());
                 mnt.free_formatted = format_count(mnt.free as f64, delimiter.get_powers_of());
                 mnt.used_formatted = format_count(mnt.used as f64, delimiter.get_powers_of());
 
@@ -248,16 +267,38 @@ fn run(args: Args) -> Result<()> {
                 let mnts: &[&MountEntry] = &mnts.iter().collect::<Vec<_>>();
                 display_mounts(&mnts, &theme, args.inodes);
             } else {
-                let mnts: &[&MountEntry] = &args.paths.iter().filter_map(|path| {
-                    path.canonicalize().map_err(|error|
-                        eprintln!("dfrs: {}: {}", path.clone().into_os_string().to_string_lossy(), error)
-                    ).ok()
-                }).map(|path| {
-                    debug!("{:?}", path);
-                    mnts.iter().map(|mnt| {
-                        (if path.starts_with(&mnt.mnt_dir) { mnt.mnt_dir.len() } else { 0 }, mnt)
-                    }).max_by_key(|x| x.0).map(|x| x.1)
-                }).filter_map(|opt| opt).collect::<Vec<_>>();
+                let mnts: &[&MountEntry] = &args
+                    .paths
+                    .iter()
+                    .filter_map(|path| {
+                        path.canonicalize()
+                            .map_err(|error| {
+                                eprintln!(
+                                    "dfrs: {}: {}",
+                                    path.clone().into_os_string().to_string_lossy(),
+                                    error
+                                )
+                            })
+                            .ok()
+                    })
+                    .map(|path| {
+                        debug!("{:?}", path);
+                        mnts.iter()
+                            .map(|mnt| {
+                                (
+                                    if path.starts_with(&mnt.mnt_dir) {
+                                        mnt.mnt_dir.len()
+                                    } else {
+                                        0
+                                    },
+                                    mnt,
+                                )
+                            })
+                            .max_by_key(|x| x.0)
+                            .map(|x| x.1)
+                    })
+                    .filter_map(|opt| opt)
+                    .collect::<Vec<_>>();
 
                 display_mounts(&mnts, &theme, args.inodes);
             }
@@ -270,14 +311,9 @@ fn run(args: Args) -> Result<()> {
 fn main() {
     let args = Args::from_args();
 
-    let logging = if args.verbose {
-        "debug"
-    } else {
-        "info"
-    };
+    let logging = if args.verbose { "debug" } else { "info" };
 
-    env_logger::init_from_env(Env::default()
-        .default_filter_or(logging));
+    env_logger::init_from_env(Env::default().default_filter_or(logging));
 
     if let Err(err) = run(args) {
         eprintln!("Error: {}", err);
